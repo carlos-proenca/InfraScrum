f9687ef5b8374f9af3d1dc74add29a66de1a7688
Carlos Eduardo <carlos_proenca@live.com>
2015-08-29 13:36:14 -0300

    corrigido as imagens

--- www/js/infracommerceController.js
+++ www/js/infracommerceController.js
@@ -1,5 +1,12 @@
 app.controller('infracommerceController', function ($scope, $state,$stateParams,$window) {
     
+
+	$scope.hours = ["1H","2H","4H","8H"];
+	$scope.days = ["2D","5D","10D"];
+	$scope.months = ["1M","2M","3M","6M"];
+	$scope.others = ["cafe","vamu"];
+
+
     $scope.getFullPage = function(choiceText){
     	 $state.go("full",{"choice":choiceText});
     }
