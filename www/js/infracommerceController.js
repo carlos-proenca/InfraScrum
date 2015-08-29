77d875027ffc912ec27ba4d7a5cab2c10f292776
Carlos Eduardo <carlos_proenca@live.com>
2015-08-29 13:55:11 -0300

    corrigido as imagens

--- www/js/infracommerceController.js
+++ www/js/infracommerceController.js
@@ -2,7 +2,7 @@ app.controller('infracommerceController', function ($scope, $state,$stateParams,
     
 
 	$scope.hours = ["1H","2H","4H","8H"];
-	$scope.days = ["2D","5D","10D"];
+	$scope.days = ["2d","5d","10d"];
 	$scope.months = ["1M","2M","3M","6M"];
 	$scope.others = ["cafe","vamu"];
 
