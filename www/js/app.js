9bdcd7e23cfac76af7589e701027e6dc8fdd1c73
Carlos Eduardo <carlos_proenca@live.com>
2016-04-04 08:17:04 -0300

    adicionado plugin para sleep

--- www/js/app.js
+++ www/js/app.js
@@ -2,29 +2,30 @@ var app = angular.module('infraScrumApp', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+	  window.plugins.insomnia.keepAwake();
+	  // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+	  // for form inputs)
+	  if(window.cordova && window.cordova.plugins.Keyboard) {
+		  cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+	  }
+	  if(window.StatusBar) {
+		  StatusBar.styleDefault();
+	  }
   });
 })
 
-app.config(function($stateProvider) {
-  $stateProvider
-
-  .state('home', {
-      url: '',
-      controller: 'homeController',
-      templateUrl: 'pages/home.html'        
-    })
-	  .state('full', {
-	    url: '/full:choice?tapOption',
-	    controller: 'fullController',
-      templateUrl: 'pages/full.html',
-      cache: false
-	  });
+app.config(function($stateProvider, $urlRouterProvider) {
+	  $urlRouterProvider.otherwise('/home');
+	  $stateProvider
+	  .state('home', {
+	      url: '',
+	      controller: 'homeController',
+	      templateUrl: 'pages/home.html'        
+	    })
+		  .state('full', {
+		    url: '/full:choice?tapOption',
+		    controller: 'fullController',
+		    templateUrl: 'pages/full.html',
+		    cache: false
+		  });
 });
